71d3f6f4cf3071a64c30b439b5dbfc9648a7495c
Felix01SA <felixsouza1200@gmail.com>
2024-11-26 18:50:32 -0300

    fix: remove debug log from discordx client

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,5 +1,4 @@
 import signale from "signale";
-import { ILogger } from "discordx";
 import { singleton } from "tsyringe";
 
 @singleton()
@@ -12,27 +11,5 @@ export class Logger extends signale.Signale {
         uppercaseLabel: true,
       },
     });
-  }
-}
-
-
-export class ClientLogger implements ILogger {
-  constructor(private readonly logger: Logger) {
-  }
-
-  error(...args: unknown[]) {
-    this.logger.error(...args);
-  }
-
-  info(...args: unknown[]): void {
-    this.logger.info(...args);
-  }
-
-  log(...args: unknown[]): void {
-    this.logger.debug(...args);
-  }
-
-  warn(...args: unknown[]): void {
-    this.logger.warn(...args);
   }
 }
